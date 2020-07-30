b98c891f184b93b78a36b697004f4b03e80860b5
wat <wat.tun.music777@gmail.com>
2020-07-30 15:02:22 +0000

    lesson15 kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
   end
 
   def show
-    unless @task
-      redirect_to root_url
-    end
   end
 
   def new
@@ -49,8 +46,11 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
+  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   # Strong Parameter
   def task_params
